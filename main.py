ff56c5b75db614606671c8056bd9157394a67f4f
anemonemon <monika.skink@gmail.com>
2024-09-30 19:22:20 +0300

    9 uzd

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import random
+import string
+
 # # kintamieji
 # # uzduotis 1
 # vardas = 'Jenna'
 # pavard = 'Ortega'
 # result = vardas + ' ' + pavard
 # print(result)
-from dataclasses import replace
 
 # uzduotis 2
 
@@ -37,12 +39,37 @@ from dataclasses import replace
 
 # # uzduotis 6
 
-filmas = 'An American in Paris'
-print(filmas.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
-filmas2 = "Breakfast at Tiffany's"
-print(filmas2.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
-filmas3 = "2001: A Space Odyssey"
-print(filmas3.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
-filmas4 = "It's a Wonderful Life"
-print(filmas4.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
+# filmas = 'An American in Paris'
+# print(filmas.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
+# filmas2 = "Breakfast at Tiffany's"
+# print(filmas2.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
+# filmas3 = "2001: A Space Odyssey"
+# print(filmas3.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
+# filmas4 = "It's a Wonderful Life"
+# print(filmas4.translate({ord(i): None for i in 'AaEeIiOoUuYy'}))
+#
 
+# uzduotis 7
+
+# starWars = "Star Wars: Episode " + ("" * random.randint(1, 9)) + str(random.randint(1, 7)) + " - A New Hope"
+# print(starWars)
+# print(starWars[19])
+#
+#
+# uzduotis 8
+
+# filmas = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
+# raides = list(map(len, filmas.split()))
+# print(raides)
+# print(raides.count(5) + raides.count(4) + raides.count(3) + raides.count(2) + raides.count(1))
+# filmas2 = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale"
+# raides2 = list(map(len, filmas2.split()))
+# print(raides2)
+# print(raides2.count(5) + raides2.count(4) + raides2.count(3) + raides2.count(2) + raides2.count(1))
+
+# uzduotis 9
+
+pirmas = random.choice(string.ascii_lowercase)
+antras = random.choice(string.ascii_lowercase)
+trecias = random.choice(string.ascii_lowercase)
+print(pirmas + antras + trecias)
